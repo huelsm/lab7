35fabfa1e7b12448ed2ab2bac9ba67b7ffe07abc
Matt Huels <mhuels@hawk.iit.edu>
2019-11-21 16:51:14 -0600

    js file

---
+++ script.js
@@ -0,0 +1,10 @@
+ function init(){
+//add your javascrip between these two lines of code
+ document.getElementById("entrybutton").addEventListener("click", funtion() 
+ { alert ("Matt Huels: This is a test.");
+ }, 
+ function2() {
+ document.get.ElementById("center").style.position = "absolute";
+ }
+ )
+window.addEventListener('load', init);
